7d35788525e80fa3f361b71819c73a25d4705040
Maria Leda Kozlowski <Leda_253@live.com.ar>
2023-02-16 00:01:03 -0300

    Proyecto CV

---
+++ js/function.js
@@ -0,0 +1,14 @@
+document.getElementById("boton1").addEventListener("click", function(){
+    document.getElementById("boton1").innerHTML="1156021844";
+});
+
+document.getElementById("boton2").addEventListener("click", function() {
+    document.getElementById("boton2").innerHTML="Leda_253@live.com.ar";
+});
+
+document.getElementById("hidden").addEventListener("click", function() {
+    document.getElementById("boton1").innerHTML="Teléfono";
+    document.getElementById("boton2").innerHTML="Email"; 
+});
+
+
